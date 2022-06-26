f3d7962154d0dd3e173c45a90b1f07d8d246e9a5
haperezv <arcaico13@hotmail.com>
2022-06-26 08:38:08 -0500

    cambio de las rutas a resource y root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'letters/', to: 'letters#index'
-  get 'letters/new'
+  resources :letters
+ # get 'letters/', to: 'letters#index'
+ # get 'letters/new'
 
-  post 'letters/', to: 'letters#create'
+ # post 'letters/', to: 'letters#create'
+
+  root 'letters#index'
 end
